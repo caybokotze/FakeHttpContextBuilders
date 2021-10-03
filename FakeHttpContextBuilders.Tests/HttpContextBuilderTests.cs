5f910a3f5e7554554084c14549150d0a4543bd8f
Caybo Kotzé <kotzecabo@gmail.com>
2021-10-03 14:35:40 +0200

    :sparkles: add full context builder test

--- FakeHttpContextBuilders.Tests/HttpContextBuilderTests.cs
+++ FakeHttpContextBuilders.Tests/HttpContextBuilderTests.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -62,7 +62,7 @@ namespace FakeHttpContextBuilders.Tests
             // arrange
             var httpContextBuilder = new HttpContextBuilder();
             // act
-            var httpContext = httpContextBuilder.WithItems(new Dictionary<object, object?>
+            var httpContext = httpContextBuilder.WithItems(new Dictionary<object, object>
                 {
                     {
                         new {Name = "Michael"},
@@ -80,7 +80,7 @@ namespace FakeHttpContextBuilders.Tests
                         {"auth", "anotherSecret1234"}
                     }))
                     .WithMethod("GET")
-                    .WithQueryString(new QueryString("?id = 23"))
+                    .WithQueryString(new QueryString("?id=1"))
                     .WithProtocol("HTTPS")
                     .WithContentLength(4)
                     .HasFormContentType(false)
@@ -117,6 +117,42 @@ namespace FakeHttpContextBuilders.Tests
             // assert
             Expect(httpContext).To.Not.Be.Null();
             Expect(httpContext.Connection.Id).To.Be.Equal.To("23");
+            Expect(httpContext.Connection.LocalPort).To.Be.Equal.To(4433);
+            Expect(httpContext.Connection.RemotePort).To.Be.Equal.To(5454);
+            Expect(httpContext.Connection.LocalIpAddress).To.Be.Equal.To(new IPAddress(new byte[]{ 192, 168, 1, 2 }));
+            Expect(httpContext.Connection.RemoteIpAddress).To.Be.Equal.To(new IPAddress(new byte[]{ 192, 168, 1, 2 }));
+            Expect(httpContext.Connection.ClientCertificate).Not.To.Be.Null();
+            Expect(httpContext.User.Claims.ToList()[0].Type).To.Equal("user");
+            Expect(httpContext.User.Claims.ToList()[0].Value).To.Equal("John");
+            Expect(httpContext.User.Claims.ToList()[1].Type).To.Equal("password");
+            Expect(httpContext.User.Claims.ToList()[1].Value).To.Equal("birthdate");
+            Expect(httpContext.Response.StatusCode).To.Equal(200);
+            Expect(httpContext.Response.Headers).To.Equal(new HeaderDictionary
+            {
+                new("auth", "secret12345"),
+                new("another", "coffee")
+            });
+            Expect(httpContext.Response.HasStarted).To.Be.True();
+            Expect(httpContext.Response.ContentLength).To.Be.Equal.To(34);
+            Expect(httpContext.Response.ContentType).To.Be.Equal.To("XML");
+            Expect(httpContext.Response.Cookies).To.Be.Deep.Equal.To(
+                new ResponseCookies(
+                new HeaderDictionary(
+                    new Dictionary<string, StringValues>()
+            {
+                {"red", "wine"}
+            }), 
+                new DefaultObjectPool<StringBuilder>(
+                    new DefaultPooledObjectPolicy<StringBuilder>())));
+            Expect(new StreamReader(httpContext.Request.Body).ReadToEnd()).To.Equal("this is an example stream");
+            Expect(httpContext.Request.Path.Value).To.Be.Equal.To("/the path to elderado");
+            Expect(httpContext.Request.Cookies["auth"]).To.Equal("secret1234");
+            Expect(httpContext.Request.Headers["auth"]).To.Equal("anotherSecret1234");
+            Expect(httpContext.Request.Method).To.Be.Equal.To("GET");
+            Expect(httpContext.Request.QueryString.Value).To.Be.Equal.To("?id=1");
+            Expect(httpContext.Request.ContentLength).To.Be.Equal.To(4);
+            Expect(httpContext.Request.Protocol).To.Be.Equal.To("HTTPS");
+            Expect(httpContext.Request.HasFormContentType).To.Be.False();
         }
     }
 }
